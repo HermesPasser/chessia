7842c4fc824fcd196bec6fedd99f563f56828d6f
Hermes Passer <dsldouglas@hotmail.com>
2021-04-08 18:40:30 +0000

    add load_board to utils module
    now we can feed the board with states which aren't its
    original, enabling us to test stuff on board/pieces
    without having the hassle of moving each piece
    alone
    
    i'm not sure where it should be, it make sense to be
    in the board since the board already has a board setup
    method but i don't think the board should be responsible
    for loading the data so the natural place would be the
    game, but depending of how i add the tests i may not
    use the game class in some tests so i leaving it
    orphaned in utils for the time being

--- utils.py
+++ utils.py
@@ -1,3 +1,40 @@
+from color import Color
+from copy import copy
+import pieces
 
 def make_2d_array(range=range(0, 1), default=0):
     return [[default for _ in range] for _ in range]
+
+
+def load_board(board, text):
+    type = dict()
+    type['♔'] = pieces.King(Color.WHITE)
+    type['♕'] = pieces.Queen(Color.WHITE)
+    type['♖'] = pieces.Rook(Color.WHITE)
+    type['♗'] = pieces.Bishop(Color.WHITE)
+    type['♘'] = pieces.Knight(Color.WHITE)
+    type['♙'] = pieces.Pawn(Color.WHITE)
+    type['♚'] = pieces.King(Color.BLACK)
+    type['♛'] = pieces.Queen(Color.BLACK)
+    type['♜'] = pieces.Rook(Color.BLACK)
+    type['♝'] = pieces.Bishop(Color.BLACK)
+    type['♞'] = pieces.Knight(Color.BLACK)
+    type['♟︎'] = pieces.Pawn(Color.BLACK)
+    
+    text = text.strip().replace("\n", '')
+    if len(text) != board.SIZE * board.SIZE -1:
+      raise Exception(f"string size ({len(text)}) does not match the board size ({board.SIZE * board.SIZE -1})")
+    
+    x = 0
+    y = 0
+    for c in text:
+      piece = type.get(c, False)
+      if not piece:
+        continue
+      
+      # [x][y] or [y][x] i don't even know anymore
+      board.set(x, y, copy(piece))
+      x += 1
+      if x == board.SIZE:
+        x = 0
+        y += 1
