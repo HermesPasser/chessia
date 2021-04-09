ce8b046dbff263d5f6c004a020ad726eff85f060
Hermes Passer <dsldouglas@hotmail.com>
2021-04-09 17:27:47 +0000

    fix load_board exception message and make it support normal chars

--- utils.py
+++ utils.py
@@ -9,21 +9,33 @@ def make_2d_array(range=range(0, 1), default=0):
 def load_board(board, text):
     type = dict()
     type['♔'] = pieces.King(Color.WHITE)
+    type['k'] = pieces.King(Color.WHITE)
     type['♕'] = pieces.Queen(Color.WHITE)
+    type['q'] = pieces.Queen(Color.WHITE)
     type['♖'] = pieces.Rook(Color.WHITE)
+    type['r'] = pieces.Rook(Color.WHITE)
     type['♗'] = pieces.Bishop(Color.WHITE)
+    type['b'] = pieces.Bishop(Color.WHITE)
     type['♘'] = pieces.Knight(Color.WHITE)
+    type['k'] = pieces.Knight(Color.WHITE)
     type['♙'] = pieces.Pawn(Color.WHITE)
+    type['p'] = pieces.Pawn(Color.WHITE)
     type['♚'] = pieces.King(Color.BLACK)
+    type['K'] = pieces.King(Color.BLACK)
     type['♛'] = pieces.Queen(Color.BLACK)
+    type['Q'] = pieces.Queen(Color.BLACK)
     type['♜'] = pieces.Rook(Color.BLACK)
+    type['R'] = pieces.Rook(Color.BLACK)
     type['♝'] = pieces.Bishop(Color.BLACK)
+    type['B'] = pieces.Bishop(Color.BLACK)
     type['♞'] = pieces.Knight(Color.BLACK)
+    type['K'] = pieces.Knight(Color.BLACK)
     type['♟︎'] = pieces.Pawn(Color.BLACK)
+    type['P'] = pieces.Pawn(Color.BLACK)
     
     text = text.strip().replace("\n", '')
     if len(text) != board.SIZE * board.SIZE -1:
-      raise Exception(f"string size ({len(text)}) does not match the board size ({board.SIZE * board.SIZE -1})")
+      raise Exception(f"string size ({len(text)}) does not match the board size ({board.SIZE * board.SIZE})")
     
     x = 0
     y = 0
