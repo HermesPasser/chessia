ee31bc290e343eae2d7a11db3048bbae2866a7f3
Hermes Passer <dsldouglas@hotmail.com>
2021-04-12 19:33:50 +0000

    prevent load_board from not resetting all squares

--- utils.py
+++ utils.py
@@ -6,6 +6,9 @@ def make_2d_array(range=range(0, 1), default=0):
     return [[default for _ in range] for _ in range]
 
 
+# Note to self: if the serialized board has k or K it will
+# throw an exception because of the check mate logic, maybe
+# we should check it and throw an error somewhere
 def load_board(board, text):
     kv = dict()
     kv['♔'] = pieces.King(Color.WHITE)
@@ -36,7 +39,7 @@ def load_board(board, text):
     text = text.strip().replace("\n", '')
     if len(text) != board.SIZE * board.SIZE:
         raise Exception(f"string size ({len(text)}) does not match the board size ({board.SIZE * board.SIZE})")
-    
+
     i = 0
     for x, y, _ in board._iterate():
         piece = kv.get(text[i], False)
@@ -44,6 +47,8 @@ def load_board(board, text):
         if piece:
             # [x][y] or [y][x] i don't even know anymore
             board.set(x, y, copy(piece))
-      
+        else: # since i may reload the board, is better i clean up the trash
+            board.set(x, y, None)
+        
         i += 1  
 
