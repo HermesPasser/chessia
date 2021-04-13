1bdc3a665b97efd6d3c3575a421a9935dc2c3ef7
Hermes Passer <dsldouglas@hotmail.com>
2021-04-13 17:15:03 +0000

    add utilities to create a list of spots from a string

--- utils.py
+++ utils.py
@@ -6,36 +6,37 @@ def make_2d_array(range=range(0, 1), default=0):
     return [[default for _ in range] for _ in range]
 
 
+kv = dict()
+kv['♔'] = pieces.King(Color.WHITE)
+kv['k']  = pieces.King(Color.WHITE)
+kv['♕'] = pieces.Queen(Color.WHITE)
+kv['q']  = pieces.Queen(Color.WHITE)
+kv['♖'] = pieces.Rook(Color.WHITE)
+kv['r']  = pieces.Rook(Color.WHITE)
+kv['♗'] = pieces.Bishop(Color.WHITE)
+kv['b']  = pieces.Bishop(Color.WHITE)
+kv['♘'] = pieces.Knight(Color.WHITE)
+kv['n']  = pieces.Knight(Color.WHITE)
+kv['♙'] = pieces.Pawn(Color.WHITE)
+kv['p']  = pieces.Pawn(Color.WHITE)
+kv['♚'] = pieces.King(Color.BLACK)
+kv['K']  = pieces.King(Color.BLACK)
+kv['♛'] = pieces.Queen(Color.BLACK)
+kv['Q']  = pieces.Queen(Color.BLACK)
+kv['♜'] = pieces.Rook(Color.BLACK)
+kv['R']  = pieces.Rook(Color.BLACK)
+kv['♝'] = pieces.Bishop(Color.BLACK)
+kv['B']  = pieces.Bishop(Color.BLACK)
+kv['♞'] = pieces.Knight(Color.BLACK)
+kv['N']  = pieces.Knight(Color.BLACK)
+kv['♟︎'] = pieces.Pawn(Color.BLACK)
+kv['P']  = pieces.Pawn(Color.BLACK)
+
+
 # Note to self: if the serialized board has k or K it will
 # throw an exception because of the check mate logic, maybe
 # we should check it and throw an error somewhere
 def load_board(board, text):
-    kv = dict()
-    kv['♔'] = pieces.King(Color.WHITE)
-    kv['k']  = pieces.King(Color.WHITE)
-    kv['♕'] = pieces.Queen(Color.WHITE)
-    kv['q']  = pieces.Queen(Color.WHITE)
-    kv['♖'] = pieces.Rook(Color.WHITE)
-    kv['r']  = pieces.Rook(Color.WHITE)
-    kv['♗'] = pieces.Bishop(Color.WHITE)
-    kv['b']  = pieces.Bishop(Color.WHITE)
-    kv['♘'] = pieces.Knight(Color.WHITE)
-    kv['n']  = pieces.Knight(Color.WHITE)
-    kv['♙'] = pieces.Pawn(Color.WHITE)
-    kv['p']  = pieces.Pawn(Color.WHITE)
-    kv['♚'] = pieces.King(Color.BLACK)
-    kv['K']  = pieces.King(Color.BLACK)
-    kv['♛'] = pieces.Queen(Color.BLACK)
-    kv['Q']  = pieces.Queen(Color.BLACK)
-    kv['♜'] = pieces.Rook(Color.BLACK)
-    kv['R']  = pieces.Rook(Color.BLACK)
-    kv['♝'] = pieces.Bishop(Color.BLACK)
-    kv['B']  = pieces.Bishop(Color.BLACK)
-    kv['♞'] = pieces.Knight(Color.BLACK)
-    kv['N']  = pieces.Knight(Color.BLACK)
-    kv['♟︎'] = pieces.Pawn(Color.BLACK)
-    kv['P']  = pieces.Pawn(Color.BLACK)
-    
     text = text.strip().replace("\n", '')
     if len(text) != board.SIZE * board.SIZE:
         raise Exception(f"string size ({len(text)}) does not match the board size ({board.SIZE * board.SIZE})")
@@ -52,3 +53,23 @@ def load_board(board, text):
         
         i += 1  
 
+
+def piece_from_char(char):
+    return copy(kv.get(char, None))
+
+
+def make_spot(text):
+    """Given a string p<x,y> where:
+
+    p is a char representation of a piece
+    x and y are digit 
+    
+    returns a tuple reprsenting a spot/square:
+    (int, int, pieces.Piece)
+    """
+    p, _, x, _, y, _ = list(text)
+    return (piece_from_char(p), int(x), int(y))
+
+
+def make_spots(*args):
+    return [make_spot(text) for text in args]
