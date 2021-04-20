27e9be8b07489035ca03333c8f997d65f5bbdc3a
Hermes Passer <dsldouglas@hotmail.com>
2021-04-20 17:58:18 -0300

    add bishop tests

--- tests.py
+++ tests.py
@@ -1,7 +1,8 @@
-from tests.board_test import BoardTests
-from tests.pawn_test import PawnTest
-from tests.knight_test import KnightTest
-from tests.rook_test import RookTest
+# from tests.board_test import BoardTests
+# from tests.pawn_test import PawnTest
+# from tests.knight_test import KnightTest
+# from tests.rook_test import RookTest
+# from tests.bishop_test import BishopTest
 import unittest
 
 if __name__ == '__main__':
