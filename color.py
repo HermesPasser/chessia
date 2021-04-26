d6137ec6e7625f874ea3f241fbe85f1df77b90e1
Douglas Lacerda <dsldouglas@hotmail.com>
2021-04-26 14:37:51 -0300

    Revert "Dev - checkmate and draw"

--- color.py
+++ color.py
@@ -1,9 +1,6 @@
 from enum import Enum
 
+# TODO: i can just make a bool of it
 class Color(Enum):
     WHITE = 1
     BLACK = 2
-
-    def reverse(self):
-        """Return the other color."""
-        return Color.BLACK if self == Color.WHITE else Color.WHITE 
