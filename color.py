3e8b7363c2094c1886ba07fa075ab531d339628e
Hermes Passer <dsldouglas@hotmail.com>
2021-04-26 13:59:17 -0300

    add utility to get the other color and remove TODO, Color enum is going to stay

--- color.py
+++ color.py
@@ -1,6 +1,9 @@
 from enum import Enum
 
-# TODO: i can just make a bool of it
 class Color(Enum):
     WHITE = 1
     BLACK = 2
+
+    def reverse(self):
+        """Return the other color."""
+        return Color.BLACK if self == Color.WHITE else Color.WHITE 
