181ea148a1f54d79d140646b72abcfd123a3e4d1
Hermes Passer <dsldouglas@hotmail.com>
2021-05-07 17:54:21 -0300

    test if moving a piece will leave the king in a check state and remove a duplicated test
    not sure if this kind of situation should lead to a ...NOT_MOVE,
    but in the way i organized it, it still throws the right message
    with the ChessException.

--- tests/game_test.py
+++ tests/game_test.py
@@ -1,4 +1,7 @@
-from engine.game import Game
+from engine.game import Game, MoveState
+from engine.color import Color
+from engine.pieces import Bishop, Rook
+from engine.position import Position
 from engine.move_result import MoveResult
 from utils import load_board
 import unittest
@@ -7,14 +10,31 @@ class GameTests(unittest.TestCase):
     def setUp(self):
         self.game = Game()
 
+    def assertMoveState(self, turn, start, end, state):
+        self.game._turn = turn       
+        result, _ = self.game._check_move_state(start, end)
+        self.assertEqual(result, state, f"Expecting {state}, given {result}")
+    
     def test_check_move_state_returns_can_be_placed(self):
         self.assertTrue(False)
-        
-    def test_check_move_state_returns_can_be_placed(self):
-        self.assertTrue(False)
-     
-    def test_check_move_state_returns_can_not_be_placed(self):
-        self.assertTrue(False)
+    
+    def test_check_move_state_returns_can_not_be_placed_if_leave_king_unprotected(self):
+        # TODO: should this kind of move really lead to a CAN_NOT_BE_PLACED?
+        """ You can't move since the bishop will left the king unprotected, if it is the rook being moved it would be fine. """
+        load_board(self.game.board, \
+            '===K====' +
+            '===P====' +
+            '========' +
+            'Q=======' +
+            '========' +
+            'r===P===' +
+            '===b====' + # right-side of bishop: 6x4
+            '====k===')
+        bishop_pos = self.game.board.get_piece_location(Color.WHITE, Bishop)
+        self.assertMoveState(Color.WHITE, bishop_pos, Position(6, 4), MoveState.CAN_NOT_BE_PLACED)
+
+        rook_pos = self.game.board.get_piece_location(Color.WHITE, Rook)
+        self.assertMoveState(Color.WHITE, rook_pos, Position(rook_pos.x, rook_pos.y + 1), MoveState.CAN_BE_PLACED)
    
     def test_check_move_state_returns_not_your_piece(self):
         self.assertTrue(False)
@@ -38,25 +58,6 @@ test the _check_move_state and _move_will_leave_in_check_state in the case
 '========' +
 '===b====' +
 '====k==='
-
-You can't move the 'b' cuzz 'k' will be in check but you can move 'r' since it will make 'k' not in check
-'===K====' +
-'===P====' +
-'========' +
-'Q=======' +
-'========' +
-'r===P===' +
-'===b====' +
-'====k==='
-
-'===K====' +
-'===P====' +
-'========' +
-'Q=======' +
-'========' +
-'====b===' +
-'========' +
-'=====k=='
 
 i think it should say the king is in check
 '===K====' +
