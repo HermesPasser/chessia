e1b902d7dd0b01a31df81eeb574e1fa81672b983
Hermes Passer <dsldouglas@hotmail.com>
2021-06-07 12:47:50 -0300

    make the ai prefer moves that put the player in checkmate or check
    to make performatic, this patch will cache whenether a player is
    checkmated for each time it enter the minimax method. The same could
    be done with the in_check in the board.

--- engine/ai.py
+++ engine/ai.py
@@ -1,11 +1,12 @@
 from engine.move_result import MoveResult
+from engine.color import Color
 import random
 import math
 # Base: byanofsky.com/2017/07/06/building-a-simple-chess-ai/
 
 random_sort = lambda a: 0.5 - random.random()
 
-def evaluate_board(board, color):
+def _evaluate_board(board, color):
     value = 0
     for r in range(board.SIZE):
         for c in range(board.SIZE):
@@ -15,9 +16,41 @@ def evaluate_board(board, color):
 
     return value
 
-def calc_best_move(depth, game, player_color,alpha=-math.inf, beta=math.inf, is_maximizing_player=True) -> (int, MoveResult):
-    if depth == 0:
-        value = evaluate_board(game.board, player_color)
+
+def _evaluate_check(board, color):
+    enemy = color.reverse()
+    check_bonus = 45
+    return check_bonus if board.in_check(enemy) else 0
+
+def _evaluate_threats(game, color, depth):
+    global _cached_white_mated, _cached_black_mated
+    enemy = color.reverse()
+    mate_bonus = 10000
+    depth_bonus = 1
+
+    enemy_mated = _cached_white_mated if enemy.is_white() else _cached_black_mated
+    return mate_bonus * depth_bonus if enemy_mated else _evaluate_check(game.board, color)
+
+
+def evaluate(game, color, depth):
+    return _evaluate_board(game.board, color) + _evaluate_threats(game, color, depth) -_evaluate_threats(game, color.reverse(), depth)
+
+# since we'll use mate to evaluate the board, why not save the processing
+# by storing the value checked to stop the search
+_cached_white_mated = None
+_cached_black_mated = None
+
+def _game_ended(game):
+    global _cached_white_mated, _cached_black_mated
+    _cached_white_mated = game._checkmated(Color.WHITE)
+    _cached_black_mated = game._checkmated(Color.BLACK)
+    return _cached_white_mated or _cached_black_mated
+
+
+def calc_best_move(depth, game, player_color,alpha=-math.inf, beta=math.inf, is_maximizing_player=True):
+    """=> (int, MoveResult)"""
+    if depth == 0 or _game_ended(game):
+        value = evaluate(game, player_color, depth)
         return [value, None]
 
     best_move = None # best move not set yet
